e9a9650941c0d34dd49bd49a4eaac8c53e2c46d7
Marina Kononova <marinakononova@mail.ru>
2024-02-27 19:08:17 +0400

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int mass, double height){
-        int bmi = (int)(mass/ Math.pow(height, 2));
+    public int calculate(int mass, double height) {
+        int bmi = (int) (mass / Math.pow(height, 2));
         return bmi;
 
         //ИМТ= вес (кг) / рост (м)2.
